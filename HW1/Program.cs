c658027734d1a4b498a293ab952d6879f6d6b47e
Андрій Копитко <yovjik89@gmail.com>
2018-09-03 23:47:30 +0300

    1

---
+++ HW1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// HW1 TASK "B"
+
+            //Console.WriteLine("Hi!\nPlease, enter an integer that matches the side of the square.");
+            //string input = Console.ReadLine();
+            //int a;
+
+            //bool check = Int32.TryParse(input, out a);
+            //if (check == true)
+            //{
+            //    int square_area = a * a;
+            //    int square_perimeter = a * 4;
+            //    Console.WriteLine("\nResults:\nArea of square = {0}\nPerimeter of square = {1}", square_area, square_perimeter);
+            //}
+            //else
+            //    Console.WriteLine("\nYou have entered not integer value. Check please and try again.");
+
+            //Console.WriteLine("\nPress any button for exit.");
+            //Console.ReadKey();
+
+            //// HW1 TASK "C"
+
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("\nHow old are you, {0}?", name);
+            //string ch_age = Console.ReadLine();
+            //int age;
+
+            //bool check_age = int.TryParse(ch_age, out age);
+            //if (check_age)
+            //    Console.WriteLine("\nNice to meet you {0}. \nYou look younger than {1}.", name, age);
+            //else
+            //    Console.WriteLine("\nYou have entered not integer value for age. Check please and try again.");
+
+            //Console.WriteLine("\nPress any button for exit.");
+            //Console.ReadKey();
+
+            // HW1 TASK "D"
+
+            //Console.WriteLine("Hi!\nPlease enter a circle radius value (decimal).");
+            //string ch_r = Console.ReadLine();
+            //double r;
+
+            //bool check_r = double.TryParse(ch_r, out r);
+            //if (check_r)
+            //{
+            //    double l = 2 * Math.PI * r;
+            //    double s = Math.PI * r * r;
+            //    double v = 4 / 3 * Math.PI * r * r * r; //Math.Pow(r,3);4f;4.0
+            //    Console.WriteLine("\nResults: \nLength = {0}\nArea = {1}\nVolume = {2}", Math.Round(l, 2), Math.Round(s, 2), Math.Round(v, 2));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You have entered not decimal value. Check please and try again.");
+            //}
+
+            //Console.WriteLine("\nPress any button for exit.");
+            //Console.ReadKey();
+        }
+    }
+}
